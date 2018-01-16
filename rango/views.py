ebaaad4b75e44dfaf85fdf22d1b0f8f422b388d7
Akshat <akshatsinghatwadia@gmail.com>
2018-01-16 23:45:03 +0530

    Exercise on URLS completed

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!")
+	return HttpResponse("Rango says hey there partner!" + '<a href="/rango/about">About.</a>')
 
 def homeofrango(request):
 	return HttpResponse("THis is my home of rango page.")
+
+def about(request):
+	return HttpResponse("Rango says here is the about page." + '<a href="/rango/">Main Page.</a>');
