7232c7508b022f1a21e4286ad018a1a1bb783a50
Akshat <akshatsinghatwadia@gmail.com>
2018-01-23 16:21:53 +0530

    Forms basics covered

--- rango/urls.py
+++ rango/urls.py
@@ -6,7 +6,8 @@ import views
 urlpatterns =[
 
 	url(r'^$', views.index, name='homeofrango'),
-	url(r'^about/', views.about, name='about'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^add_category/$', views.add_category, name='addcategory'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
 views.show_category, name='show_category'),
 
--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from rango.forms import CategoryForm
 from django.shortcuts import render
 from rango.models import category,page
 from django.http import HttpResponse
@@ -37,3 +38,17 @@ def show_category(request, category_name_slug):
 '''
 	return render(request, 'rango/category.html', context = context_dict)
 
+def add_category(request):
+	form = CategoryForm()
+
+	if request.method =='POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit = True)
+			return index(request)
+
+		else:
+			print(form.errors)
+
+	return render(request, 'rango/add_category.html', {'form': form})		
