fb7af2846725d614ef44b76c63603567d569357a
Akshat <akshatsinghatwadia@gmail.com>
2018-04-02 19:42:16 +0530

    Registering user Implemented

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from rango.models import category, page
+from rango.models import category, page, UserProfile
 
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title','category', 'url')
@@ -12,4 +12,4 @@ class CategoryAdmin(admin.ModelAdmin):
 	
 admin.site.register(category, CategoryAdmin)
 admin.site.register(page, PageAdmin)
-
+admin.site.register(UserProfile)
